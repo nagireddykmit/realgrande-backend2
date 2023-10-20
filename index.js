0c1643637fc1a1afe6b30b19e12d489f9547f4ec
nagireddykmit <73099062+nagireddykmit@users.noreply.github.com>
2023-10-20 11:14:23 +0530

    Updated Index

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const allRouter = require('./routes/allRoutes')
 dotenv.config()
 
-
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const db = module.exports =async ()=>{
     try{
