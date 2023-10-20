7c77212e8770b10eef285ea64517017dfa5f1c34
nagireddykmit <73099062+nagireddykmit@users.noreply.github.com>
2023-10-20 07:22:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const allRouter = require('./routes/allRoutes')
+dotenv.config()
+
+
+const app = express()
+
+
+const db = module.exports =()=>{
+    try{
+        mongoose.connect(process.env.DBURI, { user: process.env.DBUSERNAME, pass: process.env.DBPASSWORD, useNewUrlParser: true,    useUnifiedTopology: true })
+            console.log("MongoDB Connection is Successful")
+        } catch(error){
+            console.log(error);
+            console.log("MongoDB Connection is failed")
+        }
+    }
+db();
+ 
+
+
+app.use('/', (req, res, next) => {
+    console.log('A new request received at ' + new Date(Date.now()));
+    next();
+})
+
+
+app.use('/',allRouter)
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+})
